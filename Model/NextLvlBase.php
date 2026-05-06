95b02af71c4089952159cb0311cab75e26aec11a
Roger Serrats <rogerservic@gmail.com>
2026-05-06 08:39:47 +0200

    Cambiar a PDO

--- Model/NextLvlBase.php
+++ Model/NextLvlBase.php
@@ -6,14 +6,30 @@ class Database {
     private $user = DB_USER;
     private $pass = DB_PASS;
     private $db   = DB_NAME;
+    private $charset = 'utf8mb4';
     public $conn;
 
     public function getConnection() {
-        $this->conn = new mysqli($this->host, $this->user, $this->pass, $this->db);
+        $this->conn = null;
 
-        if ($this->conn->connect_error) {
-            die("Error de conexión: " . $this->conn->connect_error);
+        try {
+           
+            $dsn = "mysql:host=" . $this->host . ";dbname=" . $this->db . ";charset=" . $this->charset;
+            
+            
+            $opciones = [
+                PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION, 
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,       
+                PDO::ATTR_EMULATE_PREPARES   => false,                  
+            ];
+
+            $this->conn = new PDO($dsn, $this->user, $this->pass, $opciones);
+            
+        } catch (PDOException $e) {
+            
+            die("Error de conexión: " . $e->getMessage());
         }
+
         return $this->conn;
     }
 }
